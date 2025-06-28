1c06bec3f0370c397afe36ae430f32fa1b2f1520
Lavanya-Bajpai <lavanya1007bajpai@gmail.com>
2025-06-28 19:31:53 +0530

    Add files via upload

---
+++ src/translations/index.ts
@@ -0,0 +1,23 @@
+import { en } from './en';
+import { hi } from './hi';
+import { ta } from './ta';
+import { bn } from './bn';
+import { te } from './te';
+import { mr } from './mr';
+import { gu } from './gu';
+import { kn } from './kn';
+import { ml } from './ml';
+import { pa } from './pa';
+
+export const translations = {
+  en,
+  hi,
+  ta,
+  bn,
+  te,
+  mr,
+  gu,
+  kn,
+  ml,
+  pa,
+}; 
